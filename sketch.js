af4ae2e0e60ad4be5992a993724410a2238f3601
aarred7 <97483313+aarred7@users.noreply.github.com>
2022-02-03 13:11:49 -0600

    random dots version 1

---
+++ sketch.js
@@ -0,0 +1,19 @@
+let x, y;
+let r, g, b;
+
+
+function setup() {
+  createCanvas(600, 400);
+  background(0);
+}
+
+function draw() {
+  r = random (0, 255);
+  g = 0;
+  b = random (0, 255);
+  x = random (0,600);
+  y = random (0, 400);
+  noStroke();
+  fill(r, g, b, 100);
+  circle(x, y, 24);
+}
